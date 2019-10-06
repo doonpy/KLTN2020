c13013cc433ac948e5314f9273cff0ca08e766af
Poon Nguyen <nguyenduypoon@gmail.com>
2019-10-06 10:36:26 +0700

    Refactoring source

--- storage-method.js
+++ storage-method.js
@@ -1,77 +1,85 @@
+/* eslint-disable no-array-constructor */
+/* eslint-disable no-invalid-this */
+/* eslint-disable require-jsdoc */
 const FOLDER_STORAGE = `./raw-html`;
 const FOLDER_LOG = `./logs`;
-const fs = require("fs");
+const fs = require('fs');
 
 (() => {
-  fs.access(FOLDER_STORAGE, err => {
-    if (err && err.code === "ENOENT") fs.mkdirSync(FOLDER_STORAGE);
+  fs.access(FOLDER_STORAGE, (err) => {
+    if (err && err.code === 'ENOENT') fs.mkdirSync(FOLDER_STORAGE);
   });
 })();
 
-module.exports.getMainDomainFromUrl = url => {
-  let pattern = new RegExp(
-    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/gim
+module.exports.getMainDomainFromUrl = function getMainDomainFromUrl(url) {
+  const pattern = new RegExp(
+      /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/gim
   );
   return url
-    .match(pattern)
-    .toString()
-    .replace(/(http|https):\/\//, "");
+      .match(pattern)
+      .toString()
+      .replace(/(http|https):\/\//, '');
 };
 
-module.exports.createStorageFolder = url => {
+module.exports.createStorageFolder = function createStorageFolder(url) {
   return new Promise((resolve, reject) => {
-    let domain = this.getMainDomainFromUrl(url);
-    fs.mkdir(`${FOLDER_STORAGE}/${domain}`, err => {
-      err && err.code !== "EEXIST" ? reject(err) : resolve();
+    const domain = this.getMainDomainFromUrl(url);
+    fs.mkdir(`${FOLDER_STORAGE}/${domain}`, (err) => {
+      err && err.code !== 'EEXIST' ? reject(err) : resolve();
     });
   });
 };
 
-module.exports.createRawHtmlFile = (folderName, fileName, content) => {
-  fs.writeFileSync(
-    `${FOLDER_STORAGE}/${folderName}/${fileName.replace(/\//g)}.html`,
+module.exports.createRawHtmlFile = function createRawHtmlFile(
+    folderName,
+    fileName,
     content
+) {
+  fs.writeFileSync(
+      `${FOLDER_STORAGE}/${folderName}/${fileName.replace(/\//g)}.html`,
+      content
   );
 };
 
-module.exports.getRawHtmlFile = (folderName, fileName) => {
+module.exports.getRawHtmlFile = function getRawHtmlFile(folderName, fileName) {
   try {
     return fs.readFileSync(`${FOLDER_STORAGE}/${folderName}/${fileName}.html`, {
-      encoding: "utf-8"
+      encoding: 'utf-8',
     });
   } catch (err) {
-    if (err.code === "ENOENT") {
-      return "ENOENT";
+    if (err.code === 'ENOENT') {
+      return 'ENOENT';
     } else {
       throw err;
     }
   }
 };
 
-module.exports.exportLog = queueUrl => {
-  let nowDate = new Date();
+module.exports.exportLog = function exportLog(queueUrl) {
+  const nowDate = new Date();
   queueUrl.lastUpdate = nowDate;
   fs.writeFileSync(
-    `${FOLDER_LOG}/log_${this.getMainDomainFromUrl(queueUrl.mainUrl)}.json`,
-    JSON.stringify(queueUrl)
+      `${FOLDER_LOG}/log_${this.getMainDomainFromUrl(queueUrl.mainUrl)}.json`,
+      JSON.stringify(queueUrl)
   );
 };
 
-module.exports.createLog = url => {
-  let domain = this.getMainDomainFromUrl(url);
+module.exports.createLog = function createLog(url) {
+  const domain = this.getMainDomainFromUrl(url);
   try {
     return JSON.parse(
-      fs.readFileSync(`${FOLDER_LOG}/log_${domain}.json`, {
-        encoding: "utf-8"
-      })
+        fs.readFileSync(`${FOLDER_LOG}/log_${domain}.json`, {
+          encoding: 'utf-8',
+        })
     );
   } catch (err) {
-    if (err.code === "ENOENT") {
+    if (err.code === 'ENOENT') {
       return {
         mainUrl: url,
+        // eslint-disable-next-line no-array-constructor
         handled: new Array(),
         waiting: new Array(),
-        failed: new Array()
+        failed: new Array(),
       };
     } else {
       throw err;
