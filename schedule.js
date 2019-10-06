659501811fee8ad24a3bd238535790d69314c07b
Poon Nguyen <nguyenduypoon@gmail.com>
2019-10-06 10:38:56 +0700

    Refactoring source

--- schedule.js
+++ schedule.js
@@ -1,44 +1,47 @@
-const childProcess = require("child_process");
-const numCPUs = require("os").cpus().length;
+/* eslint-disable no-array-constructor */
+/* eslint-disable max-len */
+/* eslint-disable camelcase */
+const childProcess = require('child_process');
+const numCPUs = require('os').cpus().length;
 const MODUlE_PATH = `./crawler`;
 const REPEAT_TIME_DAY = 1000 * 3600 * 24;
-let crawlProcessList = new Array();
+const crawlProcessList = new Array();
 
-const childProcessCrawler = urlList => {
-  let cloneUrlList = [...urlList];
+const childProcessCrawler = (urlList) => {
+  const cloneUrlList = [...urlList];
 
   const loopUrl = setInterval(() => {
-    //prevent duplicate process
+    // prevent duplicate process
     if (crawlProcessList.length < numCPUs - 1) {
-      if (crawlProcessList.find(p => p.url === cloneUrlList[0]) !== undefined) {
+      if (crawlProcessList.find((p) => p.url === cloneUrlList[0]) !== undefined) {
         cloneUrlList.shift();
         return;
       }
-      let forked = childProcess.fork(MODUlE_PATH);
+      const forked = childProcess.fork(MODUlE_PATH);
       console.log(
-        `=> Fork child process ${forked.pid} for crawl "${cloneUrlList[0]}"`
+          `=> Fork child process ${forked.pid} for crawl "${cloneUrlList[0]}"`
       );
 
-      forked.send({ url: cloneUrlList[0] });
+      forked.send({url: cloneUrlList[0]});
       crawlProcessList.push({
         pid: forked.pid,
-        url: cloneUrlList[0]
+        url: cloneUrlList[0],
       });
       cloneUrlList.shift();
 
-      forked.on("message", data => {
+      forked.on('message', (data) => {
         if (data.type === false) {
           console.log(`=> [PID:${forked.pid}] ERR: ${data.err}`);
         }
-        forked.kill("SIGTERM");
+        forked.kill('SIGTERM');
         crawlProcessList.splice(
-          crawlProcessList.findIndex(p => {
-            p.pid === forked.pid;
-          }),
-          1
+            crawlProcessList.findIndex((p) => {
+              p.pid === forked.pid;
+            }),
+            1
         );
         console.log(
-          `=> Kill child process ${forked.pid} - status: ${forked.killed}`
+            `=> Kill child process ${forked.pid} - status: ${forked.killed}`
         );
       });
     }
@@ -46,7 +49,7 @@ const childProcessCrawler = urlList => {
     if (cloneUrlList.length === 0) {
       clearInterval(loopUrl);
     }
-  }, 0); //0 for callback exec immediately
+  }, 0); // 0 for callback exec immediately
 };
 
 module.exports.main = (urlList, repeatTime) => {
