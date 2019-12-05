4d80df3bab1af31c959532b50d0f714095eb3338
Khoa Le <43885032+khoale9098@users.noreply.github.com>
2019-12-06 01:53:06 +0700

    fix catalog

--- controllers/input-controller.js
+++ controllers/input-controller.js
@@ -6,10 +6,10 @@ const extract = require("../core/extractDetail");
 const scrape = require("../core/scrapePagination");
 const HostnameModel = require("../models/hostname-model");
 const CatalogModel = require("../models/catalog-model");
-let urlPage;
 let listCatalog = [];
-let objCatalog = [];
+
 const myMap = new Map();
+const myHost = new Map();
 let formatBody = $ => {
   $("head").append(`<style>
     .crawler-border-solid-selected { border: 2px solid #dee2e6 !important }
@@ -24,17 +24,20 @@ let getInput = (req, res) => {
 };
 
 let postURL = async (req, res) => {
-  urlPage = req.body.url;
-  let urlmodel = urlPage.split(/^https?\:\/\//i);
-  console.log(chalk.bold.red(urlmodel[1]));
+  let urlPage = req.body.url;
+  
+  let urlmodel = await urlPage.split(/^https?\:\/\//i);
+  global.urlPage = urlPage;
 
-  const countUrl = await HostnameModel.countDocuments({
-    name: urlmodel[1]
-  });
-  if (!countUrl) {
+  let result = await HostnameModel.findOne({ name: urlmodel[1] });
+  myHost.set("host", urlmodel[1]);
+  console.log(result);
+  if (!result) {
     HostnameModel.create({
       name: urlmodel[1]
     });
+  } else {
+    console.log("Already Exist!");
   }
   let options = {
     url: urlPage,
@@ -59,20 +62,21 @@ let getURL = (url, res) => {
 
 let postCatalog = async (req, res) => {
   let item = JSON.parse(req.body.catalog);
-  let urlmodel = urlPage.split(/^https?\:\/\//i);
-  const host = await HostnameModel.findOne({ name: urlmodel[1] });
-  console.log("HOST : " + host);
+  let hostname = myHost.get("host");
+  const host = await HostnameModel.findOne({ name: hostname });
+
+  let catalogs = [];
   item.forEach(async (value, index) => {
-    listCatalog.push(urlPage + value.href);
+    catalogs.push(urlPage + value.href);
     const countUrl = await CatalogModel.countDocuments({
       name: value.name,
-      link: value.href,
+      link: urlPage + value.href,
       hostId: host._id
     });
     if (!countUrl) {
       CatalogModel.create({
         name: value.name,
-        link: value.href,
+        link: urlPage + value.href,
         hostId: host._id
       })
         .then(() => {
@@ -83,48 +87,8 @@ let postCatalog = async (req, res) => {
         });
     }
   });
-  console.log(listCatalog);
-  // item.forEach((value, index) => {
-  //   let url = [];
-  //   value.targetList.forEach(el => {
-  //     if (url.includes(urlPage + el) !== true) url.push(urlPage + el);
-  //   });
-  //   if (value !== "/" && value !== "") {
-  //     if (urlPage !== undefined) {
-  //       obj = {
-  //         catalog: value.targetName,
-  //         urlCatalogs: url
-  //       };
-  //     }
-  //     if (objCatalog.includes(obj) !== true) objCatalog.push(obj);
-  //   }
-  // });
-
-  // objCatalog.forEach(el => {
-  //   listCatalog.push(el.urlCatalogs[0]);
-
-  //   el.urlCatalogs.forEach(async val => {
-  //     let name = val.split(urlPage);
-  //     console.log(val);
-  //     let catalogname = name[1].replace("/", "");
-  //     const countUrl = await CatalogModel.countDocuments({
-  //       name: catalogname,
-  //       hostId: host._id
-  //     });
-  //     if (!countUrl) {
-  //       CatalogModel.create({
-  //         name: catalogname,
-  //         hostId: host._id
-  //       });
-  //     }
-  //   });
-  // });
-  const catalogId = await CatalogModel.find({ hostnameId: host._id });
-  let resultCatalog = [];
-  resultCatalog.push(catalogId._id);
-  HostnameModel.create({
-    catalog: resultCatalog
-  });
+  listCatalog.push(catalogs[0]);
+  global.catalogs = catalogs;
   res.send("Success!");
 };
 //
@@ -166,6 +130,7 @@ let getDetailPage = async (req, res) => {
   let paginationArr = JSON.parse(req.body.paginationXpath);
   let urlDetect = listCatalog[0];
   let xPath = xpath[0];
+
   //
   // console.log(paginationArr);
   // console.log(urlDetect);
@@ -178,9 +143,12 @@ let getDetailPage = async (req, res) => {
   let pagination = await extract.getPagination(paginationXpath, urlDetect);
   console.log("Pagination : " + pagination);
   console.log(chalk.bold.blue("RUNNNNNN"));
-  // setTimeout(() => {
-  //   scrape.scrapeDetail(textClass, urlPage, pagination, urlDetect, objCatalog);
-  // }, 1000);
+  console.log(catalogs);
+  let hostname = myHost.get("host");
+  console.log(hostname);
+  setTimeout(() => {
+    scrape.scrapeDetail(textClass, urlPage, pagination, urlDetect, catalogs);
+  }, 1000);
 };
 module.exports = {
   getInput: getInput,
