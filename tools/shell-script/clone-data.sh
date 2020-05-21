a0b81234f58cfc10b675eca9935bc5922513c050
Poon Nguyen <poon-nguyen@cybozu.vn>
2020-05-21 22:08:08 +0700

    Fix bug

--- tools/shell-script/clone-data.sh
+++ tools/shell-script/clone-data.sh
@@ -6,10 +6,10 @@ MONGO_USERNAME="alice"
 MONGO_PASSWORD="alice"
 
 echo "Dumping data from $MONGO_HOST..."
-mongodump --host ${MONGO_HOST} --ssl --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} --authenticationDatabase admin --db ${MONGO_DATABASE}
+mongodump -h ${MONGO_HOST} --ssl -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --authenticationDatabase admin -d ${MONGO_DATABASE}
 
 echo "Restore data to local database..."
-mongorestore --drop -d kltn2020-dev dump/kltn2020
+mongorestore --drop --d kltn2020 dump/kltn2020
 
 echo "Remove dump folder..."
 rm -rf dump
