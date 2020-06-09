c2fa0b965afd8cabba86c1ff8cb9ae2915990aa4
Poon Nguyen <poon-nguyen@cybozu.vn>
2020-06-09 23:00:07 +0700

    Refactor analytics model

--- api-server/src/service/visual/analytics/visual.analytics.model.ts
+++ api-server/src/service/visual/analytics/visual.analytics.model.ts
@@ -5,7 +5,7 @@ import { VisualAnalyticsDocumentModel } from '@service/visual/analytics/visual.a
 
 autoIncrement.initialize(mongoose.connection);
 
-const VisualAnalysisSchema: Schema = new Schema(
+const VisualAnalyticsSchema: Schema = new Schema(
     {
         month: { type: Schema.Types.Number },
         year: { type: Schema.Types.Number },
@@ -28,23 +28,23 @@ const VisualAnalysisSchema: Schema = new Schema(
     { timestamps: { createdAt: 'cTime', updatedAt: 'mTime' } }
 );
 
-VisualAnalysisSchema.plugin(autoIncrement.plugin, {
+VisualAnalyticsSchema.plugin(autoIncrement.plugin, {
     model: 'visual_analytics',
     startAt: 1,
     incrementBy: 1,
 });
 
-VisualAnalysisSchema.index({ month: 1 }, { name: 'idx_month' });
-VisualAnalysisSchema.index({ year: 1 }, { name: 'idx_year' });
-VisualAnalysisSchema.index(
+VisualAnalyticsSchema.index({ month: 1 }, { name: 'idx_month' });
+VisualAnalyticsSchema.index({ year: 1 }, { name: 'idx_year' });
+VisualAnalyticsSchema.index(
     { transactionType: 1 },
     { name: 'idx_transactionType' }
 );
-VisualAnalysisSchema.index({ propertyType: 1 }, { name: 'idx_propertyType' });
+VisualAnalyticsSchema.index({ propertyType: 1 }, { name: 'idx_propertyType' });
 
 const VisualAnalyticsModel = mongoose.model<VisualAnalyticsDocumentModel>(
     'visual_analytics',
-    VisualAnalysisSchema
+    VisualAnalyticsSchema
 );
 
 export default VisualAnalyticsModel;
