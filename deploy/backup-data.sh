b1e9270499909889c3d31656dab8491b461d4832
Poon Nguyen <poon-nguyen@cybozu.vn>
2020-06-18 12:01:46 +0700

    Fix backup-data.sh [skip ci]

--- deploy/backup-data.sh
+++ deploy/backup-data.sh
@@ -7,21 +7,24 @@ MONGO_USERNAME="alice"
 MONGO_PASSWORD="pkroot"
 MONGO_AUTHDB="admin"
 
+WORK_DIR=/home/pkroot/deploy
 TIMESTAMP=`date +%F-%H%M`
-MONGODUMP_PATH="mongodump"
-BACKUPS_DIR=db-backup/$APP_NAME
+BACKUPS_DIR=$WORK_DIR/db-backup/$APP_NAME
 BACKUP_NAME=$APP_NAME-$TIMESTAMP
 
+DB_CONTAINER_NAME="kltn2020_db"
+DUMP_CMD="mongodump -h ${MONGO_HOST} -d ${MONGO_DATABASE} -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTHDB} -o /workdir/dump"
+
 echo "=> Start backup data..."
-$MONGODUMP_PATH -h ${MONGO_HOST} -d ${MONGO_DATABASE} -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTHDB}
+docker exec -it ${DB_CONTAINER_NAME} ${DUMP_CMD}
 
 mkdir -p ${BACKUPS_DIR}
-mv dump ${BACKUP_NAME}
+mv ${WORK_DIR}/dump ${WORK_DIR}/${BACKUP_NAME}
 
 echo "=> Zip backup data to tar..."
-tar -zcvf ${BACKUPS_DIR}/${BACKUP_NAME}.tgz ${BACKUP_NAME}
+tar -zcvf ${BACKUPS_DIR}/${BACKUP_NAME}.tgz ${WORK_DIR}/${BACKUP_NAME}
 
 echo "=> Remove dump folder..."
-rm -rf ${BACKUP_NAME}
+rm -rf ${WORK_DIR}/${BACKUP_NAME}
 
 echo "=> Done"
